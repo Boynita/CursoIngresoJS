4950d6e2ee111736d1155c10a573be7564778303
martin <boynita1995@gmail.com>
2020-07-15 12:28:32 -0300

    clase 15/07

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,28 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
-}
+	var numeroUno;
+	var numeroDos;
+	var suma;
+	
+	
+	numeroUno = txtIdNumeroUno.value;
+	numeroDos = txtIdNumeroDos.value;
+
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	
+	suma = numeroUno + numeroDos;
+	
+	//COMENTARIOS DE CALSE 15-07
+	//suma = parseInt(numeroUno) + parseInt(numeroDos);
+	//Ejemplo
+	//alert("La suma es: " (parseInt (numeroUno) + parseInt (numeroDos));
+
+	
+	
+	alert("La suma es: " + suma);
+}	
+
 
--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -6,5 +6,31 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
-	alert("ok");
+	var sueldo;
+	var aumento;
+	var resultado;
+
+	sueldo = txtIdSueldo.value;
+
+	sueldo = parseInt(sueldo);
+
+	aumento = (sueldo *10) /100;
+
+	resultado = sueldo + aumento;
+
+	txtIdResultado.value = resultado;
+
+	//EJEMPLOS (regla de 3 simples)
+	//100% -------------> sueldo
+	//10% --------------> X = 10% * sueldo = 0.1 * sueldo = 0.1 * sueldo
+	//                                100%
+	//total = sueldo + X
+
+	//100% --------------> sueldo
+	//110% --------------> X = 100% * sueldo
+
+	//total = (110* sueldo) / 100;
+	// total = 1.1 * sueldo;
+
+
 }
