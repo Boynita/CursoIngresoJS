f1e05684320e8c6d5a1d5d27e06278133d8455f8
martin <boynita1995@gmail.com>
2020-07-23 12:18:35 -0300

    Clase 23/07

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -28,30 +28,129 @@ function mostrar()
 	   */
 
 
-	   var numeroUno = 2;
-	   var numeroDos = 8;
-	   var numeroRandom;
-	   
-	   numeroRandom = Math.floor(Math.random()* numeroUno + numeroDos);
-
-	   if (numeroUno % 2 == 0 && numeroDos % 2 == 0){
-		   alert(numeroUno * numeroDos);
-	   }else {
-		if (!(numeroUno % 2 == 0 && numeroDos % 2 == 0) - numeroUno - numeroDos){
-			alert ("son impares");
-		}
-	   }
-
-		var numeroRandomUnoAlCubo = Math.pow(numeroRandomUnoAlCubo, 3);
+		//a
 		
-		if (numeroRandomUnoAlCubo / 5 *2*2*2){
-			alert (numeroRandomUnoAlCubo / 5 *2*2*2);
+		var numeroRandomUno = [2];
+		var numeroRandomDos = [10];
+		var numerosPares;
+		var numeroDivisible = [5];
+
+		numerosPares = Math.floor(Math.random()* numeroRandomUno + numeroRandomDos);
+		//c
+		numeroDivisible = Math.floor(Math.random()* numeroRandomUno / numeroRandomDos);
+		//a
+		numerosPares = parseFloat (numeroRandomDos);
+		numerosPares = parseFloat (numeroRandomUno);
+		numeroDivisible = parseFloat (numeroRandomUno);
+		numeroDivisible = parseFloat (numeroRandomDos);
+
+		if (numeroRandomUno %2==0 && numeroRandomDos %2==0){
+			alert(numeroRandomUno * numeroRandomDos);
 		}
+		//c 
+		if(numeroRandomUno /5*2*2*2 && numeroRandomDos /5*2*2*2){
+			alert(" Resultado " + numeroDivisible);
+		}
+			
 		
-		if (!(numeroUno && numeroDos && numeroRandomUnoAlCubo)){
+		
+		console.log(numeroRandomUno + numeroRandomDos);
+
+		//b
+
+		var numeroRandomTres = [5];
+		var numeroRandomCuatro = [3];
+		var numerosImpares;
+
+		numerosImpares = parseInt (numeroRandomCuatro);
+		numerosImpares = parseInt (numeroRandomTres);
+		
+
+		numerosImpares = Math.floor(Math.random()* numeroRandomTres + numeroRandomCuatro);
+
+		if (!(numeroRandomTres %2==0 && numeroRandomCuatro %2==0)){
+			alert(numeroRandomTres - numeroRandomCuatro);
+		}
+
+		
+		//d
+
+		if (!!(numeroRandomUno && numeroRandomDos && numeroRandomTres && numeroRandomCuatro)){
 			alert("No pasó nada");
 		}
-		console.log(numeroRandom && numeroRandomUnoAlCubo);
+		
+		
+
+		
+		
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	   //var numeroUno = 2;
+	   //var numeroDos = 8;
+	   //var numeroRandom;
+	   
+	   //numeroRandom = Math.floor(Math.random()* numeroUno + numeroDos);
+
+	   //if (numeroUno % 2 == 0 && numeroDos % 2 == 0){
+		//   alert(numeroUno * numeroDos);
+	   //}else {
+		//if (!(numeroUno % 2 == 0 && numeroDos % 2 == 0) - numeroUno - numeroDos){
+		//	alert ("son impares");
+		//}
+	  // }
+
+		//var numeroRandomUnoAlCubo = Math.pow(numeroRandomUnoAlCubo, 3);
+		
+		//if (numeroRandomUnoAlCubo / 5 *2*2*2){
+			//alert (numeroRandomUnoAlCubo / 5 *2*2*2);
+		//}
+		
+		//if (!(numeroUno && numeroDos && numeroRandomUnoAlCubo)){
+		//	alert("No pasó nada");
+		//}
+		//console.log(numeroRandom && numeroRandomUnoAlCubo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		
 
   }
--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -10,5 +10,80 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+     
+    //A
+    var lamparas;
+    var cantidadDeLamparas;
+    var marc;
+    var descueto;
+    var importeFinal;
+
+    mar = Marca.value;
+    cantidadDeLamparas = txtIdCantidad.value;
+    
+
+    cantidadDeLamparas = parseInt(lamparas);
+
+    if(cantidadDeLamparas >= 6){
+        txtIdprecioDescuento.value = (cantidadDeLamparas * descuento) * 0.50;
+    }else if(cantidadDeLamparas == 5 && marc == "ArgentinaLuz"){
+        txtIdprecioDescuento.value = (cantidadDeLamparas * descuento) * 0.60;
+    }else if (cantidadDeLamparas == 5){
+        txtIdprecioDescuento.value = (cantidadDeLamparas * descuento) * 0.70;
+    }else if (cantidadDeLamparas == 4 && != "ArgentinaLuz")||(cantidadDeLamparas == 4 && marc == "FelipeLamparas"))
+
+
+
+
+
+
+
+
+    var lamparas;
+    var cantidadDeLamparas;
+    var mar;
+    var descueto;
+    var importeFinal;
+
+    mar = Marca.value;
+    cantidadDeLamparas = txtIdCantidad.value;
+    
+
+    cantidadDeLamparas = parseInt(lamparas);
+
+    if(cantidadDeLamparas>=6){
+        descueto = cantidadDeLamparas * lamparas * 0.50;}            
+    else if (cantidadDeLamparas>=5 && mar == "ArgentinaLuz"){
+        descueto = cantidadDeLamparas * lamparas * 0.40;
+    }
+    else{descuento = cantidadDeLamparas * lamparas * 0.30;}
+    
+     if(cantidadDeLamparas ==4 && mar == "ArgentinaLuz" || mar == "FelipeLamparas"){
+        descuento = cantidadDeLamparas * lamparas * 0.25;
+    }else {
+        descuento = cantidadDeLamparas * lamparas * 0.20;
+    }
+    if(cantidadDeLamparas == 3 && mar == "ArgentinaLuz"){
+        descuento = cantidadDeLamparas * lamparas * 0.15;
+    }
+    if(cantidadDeLamparas ==3 && mar == "FelipeLamparas"){
+        descuento = cantidadDeLamparas * lamparas * 0.10;
+    }else{
+        descuento = cantidadDeLamparas * lamparas * 0.5;
+    } 
+    if(descuento > 120){
+        iibb = (descuento * 0.10);
+        descuento = descuento + iibb;
+        alert("Usted pagó" + iibb + " de IIBB. " );
+    }
+    descuento = txtIdprecioDescuento.value;
+
+    
 }
+    //if (condition){
+
+    //} else if (condition){
+
+    //}else if (condition){
+        
+    //}
